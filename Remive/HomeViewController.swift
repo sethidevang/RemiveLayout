e531da677a0ec7293ca7060e6555ff450d2c3324
DEVANG SETHI <sethidevangsethi@gmail.com>
2024-12-02 14:48:34 +0530

    new ui

---
+++ Remive/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Remive
+//
+//  Created by Devang IOS on 01/12/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var CureAsk: UITableViewCell!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
